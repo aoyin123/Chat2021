5861209152fdcf2ef403c8b6af3668b58a76f61b
aoyin <1437376843@qq.com>
2021-07-01 16:14:16 +0800

    "完成最小化按钮的功能，图像变化"

--- Chat2021/LogFrm/ControlBtn.cs
+++ Chat2021/LogFrm/ControlBtn.cs
@@ -13,11 +13,20 @@ namespace Chat2021.LogFrm
 {
     public partial class ControlBtn : Form
     {
+        Button button;
         #region 初始化
         public ControlBtn(Point point,Size size)
         {
             InitializeComponent();
             SetFromStyle(point, size);
+            this.Paint += DrawLine;
+            button = new Button();
+            button.BackColor = Color.White;
+            //this.Controls.Add(button);
+
+            uint ExdStyle = win32.GetWindowLong(this.Handle, win32.GWL_EXSTYLE);
+            win32.SetWindowLong(this.Handle, win32.GWL_EXSTYLE, win32.WS_EX_TRANSPARENT | win32.WS_EX_LAYERED);
+            win32.SetLayeredWindowAttributes(this.Handle, 0X000000, 125, 3);
         }
 
         private void SetFromStyle(Point point, Size size)
@@ -26,7 +35,16 @@ namespace Chat2021.LogFrm
             this.StartPosition = FormStartPosition.Manual;
             this.Location = point;
             this.Size = size;
+            this.BackColor = Color.FromArgb(0, 0, 0);
+            this.TopMost = true;
+            this.ShowInTaskbar = false;
             
+        }
+
+        private void DrawLine(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.DrawLine(new Pen(Brushes.White), this.Location, new Point(200, 200));
         }
         #endregion
     }
--- Chat2021/LogFrm/LogFrm.cs
+++ Chat2021/LogFrm/LogFrm.cs
@@ -22,7 +22,7 @@ namespace Chat2021.LogFrm
         Button closeBtn = new Button();
         PictureBox pic = new PictureBox();
         Rectangle minBtnRect = new Rectangle(new Point(1705, 0), new Size(135, 300));
-        Rectangle closeBtnRect = new Rectangle(new Point(1840, 0), new Size(80,  200));
+        Rectangle closeBtnRect = new Rectangle(new Point(1840, 0), new Size(135,  200));
         Rectangle setBtnRect = new Rectangle(new Point(1570, 0), new Size(135, 300));
         //Rectangle minBtnRect , setBtnRect;
         private Rectangle minRect = new Rectangle(new Point(0, 0), new Size(20, 20));
@@ -30,6 +30,7 @@ namespace Chat2021.LogFrm
         private Rectangle setRect = new Rectangle();
         delegate void HandlePicture(Bitmap bitmap);
         HandlePicture handlePicture;
+        ControlBtn controlBtn;
         #endregion
 
         #region Init
@@ -40,31 +41,50 @@ namespace Chat2021.LogFrm
             ControlInit();
             InitVar();
             InitEventHandler();
+            this.Show();
+            controlBtn = new ControlBtn(this.Location, this.Size);
+            controlBtn.Show();
         }
 
         private void FrmInit()
         {
             this.Size = new Size(538, 330);
-            this.BackColor = Color.White;
             this.StartPosition = FormStartPosition.CenterScreen;
             this.FormBorderStyle = FormBorderStyle.None;
+            this.BackColor = Color.White;
+            this.ShowInTaskbar = false;
+            System.IntPtr iconHandle = Resource1.Chat2021.GetHicon();
+            System.Drawing.Icon icon = Icon.FromHandle(iconHandle);
+            this.notifyIcon1.Icon = icon;
+            this.notifyIcon1.Text = "Chat2021";
+            this.notifyIcon1.Visible = true;
         }
 
         private void InitEventHandler()
         {
+            //绘制关闭按钮,设置按钮，最小化按钮
             handlePicture += DrawMinBtn;
             handlePicture += DrawCloseBtn;
             handlePicture += DrawSetBtn;
+
+            //设置最小化按钮事件
             PlayVedioPic.MouseMove += MouseMoveMinBtn;
             PlayVedioPic.MouseDown += MouseDonwMinBtn;
-            
+
+            //设置关闭按钮事件
+            //PlayVedioPic.MouseMove += MouseMoveCloseBtn;
+            //PlayVedioPic.MouseDown += MouseDownCloseBtn;
+
+
+            //设置设置按钮事件
+            //PlayVedioPic.MouseMove += MouseMoveSetBtn;
+            //PlayVedioPic.MouseDown += MouseDownSetBtn;
         }
 
         private void ControlInit()
         {
             PlayVedioPic.Width = this.Width;
             PlayVedioPic.Height = 125;
-            PlayVedioPic.BackColor = Color.Red;
         }
 
         private void InitVar()
@@ -85,27 +105,14 @@ namespace Chat2021.LogFrm
                 Console.WriteLine("DeCoding run...");
                 Bitmap oldBmp = null;
 
-
                 // 更新图片显示
                 tstRtmp.ShowBitmap show = (bmp) =>
                 {
                     this.Invoke(new MethodInvoker(() =>
                     {
                         Graphics g = this.PlayVedioPic.CreateGraphics();
-
-                        //for (int x = 0; x < 50; x++)
-                        //{
-                        //    for (int y = 0; y < 50; y++)
-                        //    {
-                        //        Color pixelColor = bmp.GetPixel(x, y);
-                        //        Color newColor = Color.FromArgb(pixelColor.R, 0, 0);
-                        //        bmp.SetPixel(x, y, newColor);
-                        //    }
-                        //}
                         handlePicture(bmp);
                         g.DrawImage(bmp, 0, 0, this.PlayVedioPic.Width, this.PlayVedioPic.Height);
-                        
-                        
                         
                         if (oldBmp != null)
                         {
@@ -135,32 +142,71 @@ namespace Chat2021.LogFrm
         #region 最小按钮的绘画
         private void DrawMinBtn(Bitmap bitmap)
         {
-            for(int i = minBtnRect.X; i < minBtnRect.Width + minBtnRect.X; i++)
+            int middleY = (minBtnRect.Y + minBtnRect.Height) / 2;
+            Point p1 = new Point(minBtnRect.X + 30, middleY);
+            Point p2 = new Point(minBtnRect.X + minBtnRect.Width - 30, middleY);
+
+            for (int i = p1.X; i < p2.X; i++)
             {
-                for(int j = minBtnRect.Y; j < minBtnRect.Height + minBtnRect.Y; j++)
+                for (int j = middleY; j < middleY + 20; j++)
                 {
-                    bitmap.SetPixel(i, j, Color.FromArgb(0, 0, 0));
+                    bitmap.SetPixel(i, j, Color.FromArgb(255, 255, 255));
                 }
             }
         }
+
+
+
+        private void DrawMinBtnShadow(Bitmap bitmap)
+        {
+            for (int i = minBtnRect.X; i < minBtnRect.X + minBtnRect.Width; i++)
+            {
+                for (int j = minBtnRect.Y; j < minBtnRect.Y + minBtnRect.Height; j++)
+                {
+                    Color color = bitmap.GetPixel(i, j);
+                    int R = color.R + 70;
+                    if (R > 255) R = 255;
+
+                    int G = color.G + 70;
+                    if (G > 255) G = 255;
+
+                    int B = color.B + 70;
+                    if (B > 255) B = 255;
+
+                    bitmap.SetPixel(i, j, Color.FromArgb(R, G, B));
+                }
+            }
+        }
+
+        
         #endregion
 
+        bool IsDrawMinBtnShadow = false;
         #region 鼠标划过最小按钮的事件
         private void MouseMoveMinBtn(object sender, MouseEventArgs e)
         {
             Point p = e.Location;
-            if(minBtnRect.Contains(p))
+            if(MinBtnRectGraphics.Contains(p))
             {
-
+                if (IsDrawMinBtnShadow == true) return;
+                handlePicture += DrawMinBtnShadow;
+                IsDrawMinBtnShadow = true;                
+            }
+            else
+            {
+                if (IsDrawMinBtnShadow == false) return;
+                handlePicture -= DrawMinBtnShadow;
+                IsDrawMinBtnShadow = false;
             }
         }
         #endregion
+        Rectangle MinBtnRectGraphics = new Rectangle(new Point(462, 0), new Size(34, 34));
 
         #region 鼠标点击最小按钮事件
         private void MouseDonwMinBtn(object sender, MouseEventArgs e)
         {
             Point p = e.Location;
-            if(minBtnRect.Contains(p))
+            if(MinBtnRectGraphics.Contains(p))
             {
                 this.Hide();
             }
@@ -170,13 +216,36 @@ namespace Chat2021.LogFrm
         #region 画关闭按钮 
         private void DrawCloseBtn(Bitmap bitmap)
         {
-            for(int i = closeBtnRect.X; i < closeBtnRect.X + closeBtnRect.Width; i++)
-            {
-                for(int j = closeBtnRect.Y; j < closeBtnRect.Y + closeBtnRect.Height; j++)
-                {
-                    bitmap.SetPixel(i, j, Color.FromArgb(0, 0, 0));
-                }
-            }
+            //int middleY = (closeBtnRect.Y + closeBtnRect.Height) / 2;
+            //Point p1 = new Point(closeBtnRect.X, middleY);
+            //Point p2 = new Point(closeBtnRect.X + closeBtnRect.Width, middleY);
+
+            //for(int i = p1.X; i< p2.X; i++)
+            //{
+            //    for (int j = middleY; j < middleY + 50; j++)
+            //    {
+            //        bitmap.SetPixel(i, j, Color.FromArgb(0, 0, 0));
+            //    }
+            //}
+
+            //for(int i = 0; i < 300; i++)
+            //{
+            //    for(int j = 0; j < 300; j++)
+            //    {
+            //        Color color = bitmap.GetPixel(i, j);
+            //        int R = color.R + 70;
+            //        if (R > 255) R = 255;
+
+            //        int G = color.G + 70;
+            //        if (G > 255) G = 255;
+
+            //        int B = color.B + 70;
+            //        if (B > 255) B = 255;
+
+            //        bitmap.SetPixel(i, j, Color.FromArgb(R, G, B));
+                //}
+            //}
+            
         }
         #endregion
 
@@ -184,15 +253,17 @@ namespace Chat2021.LogFrm
         #region 画设置按钮
         private void DrawSetBtn(Bitmap bitmap)
         {
-            for (int i = closeBtnRect.X; i < setBtnRect.X + closeBtnRect.Width; i++)
-            {
-                for (int j = closeBtnRect.Y; j < setBtnRect.Y + closeBtnRect.Height; j++)
-                {
-                    bitmap.SetPixel(i, j, Color.FromArgb(0, 0, 0));
-                }
-            }
+            //for (int i = setBtnRect.X; i < setBtnRect.X + closeBtnRect.Width; i++)
+            //{
+            //    for (int j = setBtnRect.Y; j < setBtnRect.Y + closeBtnRect.Height; j++)
+            //    {
+            //        bitmap.SetPixel(i, j, Color.FromArgb(0, 0, 0));
+            //    }
+            //}
         }
         #endregion
+
+        
 
     }
 }
